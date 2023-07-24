3b91e98e8c24698b41bde33f093a2b5c74a899d4
Brendan96 <Brendan.ballard96@yahoo.com>
2023-07-23 21:06:43 -0500

    add email form that wokrs!

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
-const app = express()
-const form = document.getElementById('.contact-form');
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'));
+app.post('/submit-form', (req, res) => {
+  // Retrieve form data
+  const name = req.body.Name;
+  const email = req.body.Email;
+  const message = req.body.Message;
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+  // Configure nodemailer with your email service credentials
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Replace with your email service
+    auth: {
+      user: 'Brendan.ballard96@yahoo.com', // Replace with your email
+      pass: 'October96', // Replace with your email password
+    },
+  });
 
+  // Email content
+  const mailOptions = {
+    from: 'Jamodrik@yahoo.com',
+    to: 'Brendan.ballard96@yahoo.com', // Replace with the recipient's email address
+    subject: 'Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
-  const formData = new FormData(form);
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email.');
+    } else {
+      console.log('Email sent:', info.response);
+      res.send('Message sent successfully.');
+    }
+  });
+});
 
-})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
